4b41e1392af75c175a6e00e81df756f2b12750b9
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-04-06 18:15:51 +0200

    added workflow for tests

--- src/test/kotlin/com/example/grepapp/GrepApplicationTests.kt
+++ src/test/kotlin/com/example/grepapp/GrepApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class GrepApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
-
+  @Test
+  fun contextLoads() {
+    assert(2 + 2 == 4)
+  }
 }
