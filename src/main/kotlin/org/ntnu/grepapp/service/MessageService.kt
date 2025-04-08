dc266522cf780adcb0b8c8f7a9b29edf22114983
Mori <anders.morille@gmail.com>
2025-04-08 15:17:07 +0200

    Contacts

--- src/main/kotlin/org/ntnu/grepapp/service/MessageService.kt
+++ src/main/kotlin/org/ntnu/grepapp/service/MessageService.kt
@@ -1,14 +1,15 @@
 package org.ntnu.grepapp.service
 
-import org.ntnu.grepapp.model.ChatMessage
-import org.ntnu.grepapp.repository.MessageRepository
 import org.apache.logging.log4j.LogManager
 import org.ntnu.grepapp.dto.chat.SendRequest
+import org.ntnu.grepapp.model.ChatContact
+import org.ntnu.grepapp.model.ChatMessage
+import org.ntnu.grepapp.repository.MessageRepository
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
-class MessageService (
+class MessageService(
     private val repository: MessageRepository,
     private val authService: AuthService
 ) {
@@ -33,5 +34,9 @@ class MessageService (
 
     fun getHistory(page: Pageable, recipientId: String): List<ChatMessage> {
         return repository.getList(page, recipientId, authService.getCurrentUser());
+    }
+
+    fun getContacts(page: Pageable): List<ChatContact> {
+        return repository.getContacts(page, authService.getCurrentUser());
     }
 }
