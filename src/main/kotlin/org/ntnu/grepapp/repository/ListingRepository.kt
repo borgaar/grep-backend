fb17184fe142aef25e74f086c6aa675253fa8c98
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-04-09 16:01:56 +0200

    Added query empty check to listing search

--- src/main/kotlin/org/ntnu/grepapp/repository/ListingRepository.kt
+++ src/main/kotlin/org/ntnu/grepapp/repository/ListingRepository.kt
@@ -89,7 +89,7 @@ class ListingRepository(
 
         var keywordSearchWhere = " AND ("
         var keywords: List<String> = ArrayList();
-        if (filter.searchQuery != null) {
+        if (!filter.searchQuery.isNullOrEmpty()) {
             keywords = filter.searchQuery.split(" ");
             keywords.forEach({
                 keywordSearchWhere += "l.title LIKE ? OR l.description LIKE ? OR "
